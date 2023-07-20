7272ccb320bf282571e8e0fd95501b299f9fb86d
Anton Savelev <dpssinet@gmail.com>
2023-07-20 17:52:16 +0300

    add types description

---
+++ src/types/offerFull.ts
@@ -0,0 +1,16 @@
+import {TOffer} from './offer';
+
+export type TOfferFull = TOffer & {
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: {
+    name: string;
+    avatarUrl: string;
+    isPro: boolean;
+  };
+  images: string[];
+  maxAdults: number;
+}
+
+export type TOffersFull = TOfferFull[]
